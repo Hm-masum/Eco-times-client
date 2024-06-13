bcd30e367a59d3bb5e87885dfa64f93ac775372d
Hm-masum <hhmasum88@gmail.com>
2024-06-13 21:20:10 +0600

    Register auth commit

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,51 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import imgBg from "../../assets/login.png"
+import { Link, useNavigate } from "react-router-dom";
+import imgBg from "../../assets/login.png";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import useAuth from "../../Hooks/useAuth";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { FileInput, Label } from "flowbite-react";
 
 const Register = () => {
-    const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const { createUser, updateUserProfile, setLoading } = useAuth();
+  const navigate = useNavigate();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const image = form.image.files[0];
+    const formData = new FormData();
+    formData.append("image", image);
+
+    try {
+      setLoading(true);
+      // 1.upload img and get url
+      const { data } = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${
+          import.meta.env.VITE_IMG_BB_API_KEY
+        }`,
+        formData
+      );
+
+      // sign up
+      const result = await createUser(email, password);
+      console.log(result);
+
+      // save username and photo
+      await updateUserProfile(name, data.data.display_url);
+      navigate("/");
+      toast.success("sign up successful");
+    } catch (err) {
+      console.log(err);
+      toast.error("sign up not successful");
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="flex md:flex-row-reverse flex-col items-center justify-center px-3 md:px-24">
@@ -18,7 +58,7 @@ const Register = () => {
           Register Please!
         </h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="space-y-4">
             <div>
               <label className="block mb text-sm">Your Name</label>
@@ -63,6 +103,13 @@ const Register = () => {
                   {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
                 </span>
               </div>
+            </div>
+
+            <div>
+              <div className="mb-2 block">
+                <Label value="Upload file" />
+              </div>
+              <FileInput name="image" />
             </div>
 
             <div>
